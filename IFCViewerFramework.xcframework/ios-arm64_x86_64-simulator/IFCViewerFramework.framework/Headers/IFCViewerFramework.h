cb54d4ac1308b0e24b1c0b7b2d681ffaa851f64e
Zine Essafi BEN ALI <Zine-Essafi.BEN-ALI-ext@egis.fr>
2021-06-22 15:38:43 +0200

    add new version

---
+++ IFCViewerFramework.xcframework/ios-arm64_x86_64-simulator/IFCViewerFramework.framework/Headers/IFCViewerFramework.h
@@ -0,0 +1,18 @@
+//
+//  IFCViewerFramework.h
+//  IFCViewerFramework
+//
+//  Created by Borhene Fhima on 05/05/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IFCViewerFramework.
+FOUNDATION_EXPORT double IFCViewerFrameworkVersionNumber;
+
+//! Project version string for IFCViewerFramework.
+FOUNDATION_EXPORT const unsigned char IFCViewerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IFCViewerFramework/PublicHeader.h>
+
+
